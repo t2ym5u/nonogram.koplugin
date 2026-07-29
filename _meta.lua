0670ade2b5237375e76ca41fd2ae8f2d670d296c
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-29 10:21:09 +0200

    chore: bump version to 1.1.9

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "nonogram",
     fullname    = _("Nonogram"),
     description = _("Fill cells by row and column clues to reveal the hidden picture."),
-    version     = "1.1.8",
+    version     = "1.1.9",
 }
